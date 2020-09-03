d7fef2491929fa3d95f7fe2a5809e84e3fcfa25c
Henry Zarza <henryacz7@gmail.com>
2020-09-03 18:47:21 -0500

    Starter files

---
+++ face-recognition-shopping/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <main className='container'>
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+    </main>
+  );
+}
+
+export default App;
